e413f29356cd2ab4bc0c2376f3ee8be28ebd4aaa
Dmitry Zatulin <d.zatulin@gmail.com>
2023-04-25 17:51:57 +0300

    update

--- elastic-cloud/main.tf
+++ elastic-cloud/main.tf
@@ -1,3 +1,13 @@
+terraform {
+    required_version = ">= 0.12.29"
+
+    required_providers {
+        ec = {
+            source  = "elastic/ec"
+            version = "0.4.0"
+        }
+    }
+}
 #Create SG for PrivateLink
 resource "aws_security_group" "private-link" {
     name   = var.aws_resource_name
